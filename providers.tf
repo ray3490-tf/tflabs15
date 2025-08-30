524b754cfe7d7b2548b405c8a3051d69ad0efbff
Ramon <Ramoncarreras@gmail.com>
2025-08-30 14:49:41 +0100

    removing backend

--- providers.tf
+++ providers.tf
@@ -7,7 +7,10 @@ terraform {
   }
 
   backend "azurerm" {
-    
+        resource_group_name   = "azDevTFTestrg01"
+        storage_account_name  = "racrazdevopstest01"
+        container_name        = "githubtfstate"
+        key                   = "dev.terraform.tfstate"
   }
 }
 
